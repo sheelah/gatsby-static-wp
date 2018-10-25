22f804266d21d084fcc6b69af2ed801ca311c911
Sheelah Brennan <slb53@hotmail.com>
2018-10-25 12:51:47 -0600

    Add initial WP source plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'gatsby-starter-default',
         short_name: 'starter',
@@ -17,5 +17,35 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        baseUrl: 'sheelahb.com',
+        protocol: 'https',
+        hostingWPCOM: false,
+        useACF: false,
+        verboseOutput: true,
+        // Set how many pages are retrieved per API request.
+        perPage: 100,
+        // Set how many simultaneous requests are sent at once.
+        concurrentRequests: 10,
+        includedRoutes: [
+          '/*/*/categories',
+          '/*/*/media',
+          '/*/*/pages',
+          '/*/*/portfolio',
+          '/*/*/posts',
+          '/*/*/tags',
+          '/*/*/taxonomies',
+          '/*/*/users',
+        ],
+        // Blacklisted routes using glob patterns
+        excludedRoutes: [],
+        // use a custom normalizer which is applied after the built-in ones.
+        normalizer: function({ entities }) {
+          return entities
+        },
+      },
+    },
   ],
 }
