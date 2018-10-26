a4318a1d289b4c94b3ea05479fbb30df4e526817
Sheelah Brennan <slb53@hotmail.com>
2018-10-26 12:43:45 -0600

    Generate individual blog post pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,46 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const _ = require(`lodash`);
+const Promise = require(`bluebird`);
+const path = require(`path`);
+const slash = require(`slash`);
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return new Promise((resolve, reject) => {
+    graphql(
+      `
+        {
+          allWordpressPost {
+            edges {
+              node {
+                id
+                slug
+                status
+                template
+                format
+              }
+            }
+          }
+        }
+      `
+    ).then(result => {
+      if (result.errors) {
+        console.log(result.errors);
+        reject(result.errors);
+      }
+      const postTemplate = path.resolve(`./src/templates/post.js`);
+      _.each(result.data.allWordpressPost.edges, edge => {
+        createPage({
+          path: `blog/${edge.node.slug}`,
+          component: slash(postTemplate),
+          context: {
+            id: edge.node.id,
+          },
+        });
+      });
+      resolve();
+    });
+  });
+};
